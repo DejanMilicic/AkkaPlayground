6d95e023ecaf05827eb9f37c85f5e9cc2aaf2eb6
Dejan Milicic <dejan.milicic@gmail.com>
2023-12-03 16:56:18 +0100

    wip

--- 02.fsx
+++ 02.fsx
@@ -9,7 +9,7 @@ open Akka.Actor
 
 let system = System.create "my-system" <| Configuration.defaultConfig ()
 
-//-----------------------------------------------------
+//=====================================================
 let accumulator =
     spawnAnonymous system
     <| props (fun mailbox ->
@@ -25,4 +25,42 @@ let accumulator =
 
         loop 0)
 
-accumulator <! -1
+accumulator <! 1
+
+//=====================================================
+
+let accumulator2Behavior (mailbox: Actor<int>) =
+    let rec loop sum =
+        actor {
+            let! msg = mailbox.Receive()
+
+            let newSum = sum + msg
+            printfn $"Sum = {newSum}"
+
+            return! loop newSum
+        }
+
+    loop 0
+
+let accumulator2 = spawnAnonymous system (props accumulator2Behavior)
+
+accumulator2 <! 1
+
+//=====================================================
+
+let accumulator3Behavior (initState: int) (mailbox: Actor<int>) =
+    let rec loop sum =
+        actor {
+            let! msg = mailbox.Receive()
+
+            let newSum = sum + msg
+            printfn $"Sum = {newSum}"
+
+            return! loop newSum
+        }
+
+    loop initState
+
+let accumulator3 = spawnAnonymous system (props (accumulator3Behavior 555))
+
+accumulator3 <! 1
