0f83feb7d577313dd21a6dfa94b92a0cca4dac4c
Dejan Milicic <dejan.milicic@gmail.com>
2023-12-03 12:19:19 +0100

    wip

--- 01.fsx
+++ 01.fsx
@@ -2,6 +2,7 @@
 // https://www.seventeencups.net/posts/building-a-mud-with-f-sharp-and-akka-net-part-one/
 //  https://github.com/17cupsofcoffee/AkkaMUD
 // https://github.com/akkadotnet/akka.net/blob/dev/src/examples/FSharp.Api/Greeter.fs
+// https://github.com/object/akkling-net-fest
 
 #r "nuget: Akka.Serialization.Hyperion"
 #r "nuget: Akkling"
@@ -10,42 +11,74 @@ open System
 open Akkling
 open Akka.Actor
 
-// first, create a system
-let system = Configuration.defaultConfig() |> System.create "system"
+// create a top level actor "System"
+// https://getakka.net/articles/intro/getting-started/tutorial-1.html
+let system = Configuration.defaultConfig () |> System.create "my-system"
 
-// messages that the greeter actor can handle
-type GreeterMsg =
-    | Hello of string
-    | Goodbye of string
+// definition of message that our actor can receive
+type Message =
+    | Greet of string
+    | Hi
+
+//=====================================================
+
+// basic actor that will be able to accept messages of type Message
+// and react to them by printing out a greeting
 
 // define actor and spawn it into the system
-let greeter = spawn system "greeter" <| props(fun mailbox ->
-    let rec loop() = actor {
-        let! msg = mailbox.Receive()
+let greetingActor = //                                      greetingActor - this is reference to an actor
+    spawnAnonymous system //                                spawnAnonymous - will create an actor that has no name
+    <| props (fun mailbox -> //                             props - define behavior of the actor
+        let rec loop () = //                                loop - define recursive loop of the actor
+            actor { //                                      actor - ActorBuilder that will build an actor from message handler
+                let! msg = mailbox.Receive() //             Receive - fetch next message from mailbox, if any
 
-        match (msg : GreeterMsg) with
-        | Hello name -> printf $"Hello, {name}\n"
-        | Goodbye name -> printf $"Goodbye, {name}\n"
+                match msg with //                           thanx to F# type inference, this will be stronly typed
+                | Greet name -> printfn $"Hello {name}"
+                | Hi -> printfn "Hello from F#!"
 
-        return! loop()
-    }
-    loop())
+                return! loop () //                          return! - will return result of the actor, and continue with the loop
+            }
+
+        loop ()) //                                         call loop() to start the actor
 
 // send messages to the actor
-greeter <! Hello "Joe"
-greeter <! Goodbye "Joe"
+greetingActor <! Greet "Joe"
+greetingActor <! Hi
+
+//=====================================================
+
+// spinning up an actor can be shortened
+
+// define message handler with same behavior
+let handler msg =
+    match msg with
+    | Greet name -> printfn $"Hello {name}"
+    | Hi -> printfn "Hello from F#!"
+    |> ignored //                               instead of unit, return Actor Effect
+
+// actorOf is a helper that will wrap up message handler into an actor
+// adding mailbox.Receive(), loop() and other stuff
+let greetingActor2 = spawnAnonymous system <| props (actorOf handler)
+
+greetingActor2 <! Hi
+greetingActor2 <! Greet "Jane"
+
+//=====================================================
 
 // or, define explicit behavior loop of an actor
-let greeterBehavior (mailbox: Actor<GreeterMsg>) =
-    let rec loop () = actor {
-        let! msg = mailbox.Receive ()
+let greeterBehavior (mailbox: Actor<Message>) =
+    let rec loop () =
+        actor {
+            let! msg = mailbox.Receive()
 
-        match (msg : GreeterMsg) with
-        | Hello name -> printf $"Hello, {name}\n"
-        | Goodbye name -> printf $"Goodbye, {name}\n"
+            match msg with
+            | Greet name -> printfn $"Hello {name}"
+            | Hi -> printfn "Hello from F#!"
 
-        return! loop()
-    }
+            return! loop ()
+        }
+
     loop ()
 
 // and spawn actor using it
@@ -55,7 +88,16 @@ let greeterRef = spawnAnonymous system (props greeterBehavior)
 greeterRef <! Hello "Joe"
 greeterRef <! Goodbye "Joe"
 
+// creating named actor
+// let greetingActor2 = spawn system "greetingActor2" <| props (actorOf handler)
+
 (*
+
+// stop
+// unhandled
+// loop or not
+// above loop() you can initialize code - use it later on with stateful actors
+
 let behavior (m:Actor<_>) =
     let rec loop () = actor {
         let! msg = m.Receive ()
@@ -67,4 +109,18 @@ let behavior (m:Actor<_>) =
             return! loop ()
     }
     loop ()
+*)
+
+(*
+let greeter = spawn system "greeter" <| props(fun mailbox ->
+    let rec loop() = actor {
+        let! msg = mailbox.Receive()
+
+        match (msg : Message) with
+        | Hello name -> printf $"Hello, {name}\n"
+        | Goodbye name -> printf $"Goodbye, {name}\n"
+
+        return! loop()
+    }
+    loop())    
 *)
