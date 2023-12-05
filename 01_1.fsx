01aabab6f7f2390246645e187122bf026cfc5782
Dejan Milicic <dejan.milicic@gmail.com>
2023-12-05 13:19:04 +0100

    ask

---
+++ 01_1.fsx
@@ -0,0 +1,32 @@
+// asking instead of telling
+
+// besides telling actors what to do, by sending commands
+// you can also ask actors for information, by sending queries
+
+#r "nuget: Akka.Serialization.Hyperion"
+#r "nuget: Akkling"
+
+open System
+open Akkling
+
+let system = System.create "my-system" <| Configuration.defaultConfig ()
+
+let greeterBehavior (mailbox: Actor<string>) = // we can define behavior of the actor in a separate function
+    let rec loop () =
+        actor {
+            let! msg = mailbox.Receive()
+
+            mailbox.Sender() <! $"Hello, {msg}" // replying back to the sender
+
+            return! loop ()
+        }
+
+    loop ()
+
+let greeter = spawnAnonymous system <| props greeterBehavior // spawn actor from behavior
+
+let askGreeter name : string =
+    greeter <? name |> Async.RunSynchronously
+
+askGreeter "John"
+askGreeter "Paul"
