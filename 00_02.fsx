9754f404d428f70ac8135427c60e4d3df99a8d0d
Dejan Milicic <dejan.milicic@gmail.com>
2023-12-05 14:22:16 +0100

    strongly typed actor

---
+++ 00_02.fsx
@@ -0,0 +1,33 @@
+// Strongly typed actor
+
+// You can send strongly typed messages to the actor
+
+#r "nuget: Akkling"
+
+open System
+open Akkling
+
+type Message =
+    | Greet of string
+    | Hi
+
+let system = System.create "my-system" <| Configuration.defaultConfig ()
+
+let greetingActor = //
+    spawnAnonymous system //
+    <| props (fun (mailbox: Actor<Message>) ->
+        let rec loop () =
+            actor {
+                let! msg = mailbox.Receive()
+
+                match msg with
+                | Hi -> printfn "Hello from F#!"
+                | Greet name -> printfn $"Hello {name}"
+
+                return! loop ()
+            }
+
+        loop ())
+
+greetingActor <! Hi
+greetingActor <! Greet "Jane"
