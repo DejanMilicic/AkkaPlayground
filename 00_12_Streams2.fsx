6d7540ecfc9bc4706265c88c4dc79dece703c47b
Dejan Milicic <dejan.milicic@gmail.com>
2023-12-09 00:45:19 +0100

    wip

--- 00_12_Streams2.fsx
+++ 00_12_Streams2.fsx
@@ -21,14 +21,19 @@ let source = [| 1; 2; 3; 4; 5 |] |> Source.From
 
 let source2 = Enumerable.Range(1, 100) |> Source.From
 
+// print out first 100 numbers
+
 Enumerable.Range(1, 100)
 |> Source.From
 |> Source.runForEach mat (fun x -> printfn $"{x}")
 
-//==========================================================
+// calculate factorials 1-100 and store them to a file
 
 Enumerable.Range(1, 100)
 |> Source.From
 |> Source.scan (fun acc next -> acc * bigint next) (bigint 1)
+|> Source.skip 1 // since scan will output initial value as well
 |> Source.map (fun x -> Akka.IO.ByteString.FromString(x.ToString() + "\n"))
 |> Source.runWith mat (Sink.toFile "c:\\temp\\factorials.txt")
+
+//================================================================================
