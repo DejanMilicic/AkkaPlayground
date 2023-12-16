a879530d8ccafe1b10fba56f6f1c68f8079f618b
Dejan Milicic <dejan.milicic@gmail.com>
2023-12-16 23:43:59 +0100

    wip

--- stream-temp3.fsx
+++ stream-temp3.fsx
@@ -13,11 +13,11 @@ let system = System.create "streams-sys" <| Configuration.defaultConfig ()
 
 let mat = system.Materializer()
 
-
 let actor targetRef (m: Actor<_>) =
     let rec loop () =
         actor {
             let! msg = m.Receive()
+            printf "Actor Received: %s\n\n" msg
             targetRef <! msg + "!!!"
             return! loop ()
         }
@@ -30,7 +30,8 @@ let spawnActor targetRef =
 let s =
     Source.actorRef OverflowStrategy.DropNew 1000
     |> Source.mapMaterializedValue (spawnActor)
-    |> Source.toMat (Sink.forEach (fun s -> printfn "Received: %s" s)) Keep.left
+    |> Source.toMat (Sink.forEach (fun s -> printfn $"Actor Returned: {s}\n\n")) Keep.left
+    //|> Source.toMat Sink.ignore Keep.left
     |> Graph.run mat
 
 s <! "Boo"
