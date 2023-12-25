0ff5ad938ffe7cd256558d7b0cf59a6c61519176
Dejan Milicic <dejan.milicic@gmail.com>
2023-12-25 13:44:46 +0100

    notifier

--- notifier.fsx
+++ notifier.fsx
@@ -23,19 +23,41 @@ type Region = string
 
 type RegionalEvent = { EventDay: EventDay; Region: Region }
 
+type Subscriber = string
+
+type RegionalEventSubscriber =
+    { EventDay: EventDay
+      Region: Region
+      Subscriber: Subscriber }
+
 let xMasDay = new DateTime(2023, 12, 25)
 let xMasDay2 = new DateTime(2023, 12, 26)
 
-let findRegions: Flow<EventDay, RegionalEvent, NotUsed> =
+let findRegions: Flow<EventDay, RegionalEvent seq, NotUsed> =
     Flow.Create<EventDay>()
     |> Flow.map (fun eventDay ->
-        { EventDay = eventDay
-          Region = "France" })
+        seq {
+            { EventDay = eventDay
+              Region = "France" }
+        })
 
-let mySink = Sink.forEach (fun (x: RegionalEvent) -> printfn "woohoo")
+let findSubscribers: Flow<RegionalEvent seq, RegionalEventSubscriber seq, NotUsed> =
+    Flow.Create<RegionalEvent seq>()
+    |> Flow.map (fun regionalEvents ->
+        regionalEvents
+        |> Seq.map (fun regionalEvent ->
+            { EventDay = regionalEvent.EventDay
+              Region = regionalEvent.Region
+              Subscriber = "John Doe" }))
+
+
+let mySink = Sink.forEach (fun (x: RegionalEventSubscriber seq) -> 
+    x |> Seq.iter (fun (x: RegionalEventSubscriber) -> 
+        printfn "%A" x))
 
 Source.ofSeq [ xMasDay; xMasDay2 ]
 |> Source.via findRegions
+|> Source.via findSubscribers
 |> Source.toSink mySink
 |> Graph.runnable
 |> Graph.run mat
