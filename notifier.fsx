cd65bff40bc4c6405082e5960c16014e4907b756
Dejan Milicic <dejan.milicic@gmail.com>
2023-12-25 23:32:28 +0100

    refactoring

--- notifier.fsx
+++ notifier.fsx
@@ -38,28 +38,33 @@ let xMasDay2 =
     { Name = "Christmas Day 2"
       Date = new DateTime(2023, 12, 26) }
 
-let findRegions: Flow<Event, RegionalEvent seq, NotUsed> =
-    Flow.Create<Event>()
-    |> Flow.map (fun eventDay -> seq { { Event = eventDay; Region = "France" } })
-
-let scheduler sendNotification findSubscribers =
+let scheduler
+    (regionsFinder: Event -> RegionalEvent seq)
+    (subscribersFinder: RegionalEvent seq -> RegionalEventSubscriber seq)
+    (notificationsSender: RegionalEventSubscriber seq -> unit)
+    =
     Source.ofSeq [ xMasDay; xMasDay2 ]
-    |> Source.via findRegions
-    |> Source.via (Flow.Create<RegionalEvent seq>() |> Flow.map findSubscribers)
-    |> Source.toMat (Sink.forEach sendNotification) Keep.none
+    |> Source.via (Flow.Create<Event>() |> Flow.map regionsFinder)
+    |> Source.via (Flow.Create<RegionalEvent seq>() |> Flow.map subscribersFinder)
+    |> Source.toMat (Sink.forEach notificationsSender) Keep.none
     |> Graph.runnable
     |> Graph.run mat
 
-let sendNotification (subs: RegionalEventSubscriber seq) =
-    subs
-    |> Seq.iter (fun sub -> printfn $"Sending notification about event '{sub.Event.Name}' to '{sub.Subscriber}'")
-    |> ignore
+//===============================================
 
-let findSubscribers (regionalEvents: RegionalEvent seq) =
+let regionsFinder (event: Event) : RegionalEvent seq =
+    seq { { Event = event; Region = "France" } }
+
+let subscribersFinder (regionalEvents: RegionalEvent seq) : RegionalEventSubscriber seq =
     regionalEvents
     |> Seq.map (fun regionalEvent ->
         { Event = regionalEvent.Event
           Region = regionalEvent.Region
           Subscriber = "John Doe" })
 
-scheduler sendNotification findSubscribers |> ignore
+let notificationsSender (subs: RegionalEventSubscriber seq) : unit =
+    subs
+    |> Seq.iter (fun sub -> printfn $"Sending notification about event '{sub.Event.Name}' to '{sub.Subscriber}'")
+    |> ignore
+
+scheduler regionsFinder subscribersFinder notificationsSender |> ignore
