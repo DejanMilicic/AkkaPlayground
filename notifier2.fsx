d72f78770ff58565f161ee48b69b828d00ab2891
Dejan Milicic <dejan.milicic@gmail.com>
2024-01-04 17:17:25 +0100

    wip

--- notifier2.fsx
+++ notifier2.fsx
@@ -7,6 +7,12 @@ open Akkling
 open System
 open Akka
 open Akka.Actor
+open Akka.Streams.Dsl
+open System
+open Akka.Streams
+open Akka.Streams.Dsl
+open Akkling
+open Akkling.Streams
 
 let system = System.create "scheduler" <| Configuration.defaultConfig ()
 
@@ -15,7 +21,7 @@ let system = System.create "scheduler" <| Configuration.defaultConfig ()
 type TriggerActorMsg =
     | StartEmittingMessage
     | StopEmittingMessage
-    | EmitMessage of string
+    | EmitMessage of DateOnly
 
 let emittingActor (mailbox: Actor<TriggerActorMsg>) =
     let rec loop (cancelable: Option<ICancelable>) =
@@ -31,24 +37,39 @@ let emittingActor (mailbox: Actor<TriggerActorMsg>) =
                         TimeSpan.FromSeconds(1.0),
                         TimeSpan.FromSeconds(1.0),
                         mailbox.UntypedContext.Self,
-                        EmitMessage "tick",
-                        Nobody.Instance
+                        EmitMessage(DateOnly.FromDateTime(DateTime.Now)),
+                        mailbox.UntypedContext.Sender
                     )
 
                 return! loop (Some newCancelable)
             | StopEmittingMessage ->
                 cancelable |> Option.iter (fun c -> c.Cancel())
                 return! loop None
-            | EmitMessage msg ->
-                printfn $"Emitting message {msg}"
+            | EmitMessage date ->
+                mailbox.UntypedContext.Sender.Tell date
                 return! loop cancelable
         }
 
     loop None
 
-let myActor = spawnAnonymous system <| props emittingActor
+let emittingActorRef = spawnAnonymous system <| props emittingActor
 
 
-myActor <! StartEmittingMessage
+emittingActorRef <! StartEmittingMessage
 
-myActor <! StopEmittingMessage
+emittingActorRef <! StopEmittingMessage
+
+
+//=================================================
+
+let flow: Flow<TriggerActorMsg, obj, NotUsed> =
+    Flow.Create<TriggerActorMsg, NotUsed>()
+    |> Flow.map (fun _ -> StartEmittingMessage)
+    |> Flow.ask (TimeSpan.FromSeconds 3.0) emittingActorRef
+
+let source = Source.singleton StartEmittingMessage
+
+source
+    .Via(flow)
+    .To(Sink.forEach (fun x -> printfn $"Received: {x}"))
+    .Run(system)
